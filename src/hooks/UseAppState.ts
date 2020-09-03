d637cc2d9ef1bc3874ec0f795ffc89c3350f7353
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-09-03 13:29:04 +0300

    Fix folder name

---
+++ src/hooks/UseAppState.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import { AppState, AppStateStatus } from 'react-native'
+
+function useAppState() {
+  const [appState, setAppState] = useState(AppState.currentState)
+
+  useEffect(() => {
+    const handleAppStateChange = (nextAppState: AppStateStatus) => {
+      setAppState(nextAppState)
+    }
+
+    AppState.addEventListener('change', handleAppStateChange)
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange)
+    }
+  }, [])
+
+  return appState
+}
+
+export default useAppState
