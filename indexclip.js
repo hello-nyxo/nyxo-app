6cff9903a24af62aa227e17cea9964c9c89efefe
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-12-13 14:03:20 +0200

    Work on app clip

--- indexclip.js
+++ indexclip.js
@@ -2,14 +2,15 @@ import { AppRegistry, SafeAreaView } from 'react-native'
 import React from 'react'
 import { ThemeProvider } from 'styled-components/native'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { CONFIG } from '@config/Config'
 import { AppClip } from './src/AppClip'
 import { lightTheme } from './src/styles/themes'
 
-const TOKEN = ''
-const SPACE = ''
+const TOKEN = CONFIG.CONTENTFUL_SPACE_ACCESS_TOKEN
+const SPACE = CONFIG.CONTENTFUL_SPACE
 
 const client = new ApolloClient({
-  uri: 'https://graphql.contentful.com/content/v1/spaces/2bj8pq0ood89',
+  uri: `https://graphql.contentful.com/content/v1/spaces/${SPACE}`,
   headers: {
     authorization: `Bearer ${TOKEN}`,
     'Content-Language': 'en-us'
